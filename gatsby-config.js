eb5136d7fbbbd3247812e44e4d4cf17c2dc8f193
william-shelly <william-shelly@users.noreply.github.com>
2021-03-16 19:01:52 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "Kilkennys",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "R_6Rq5Q3OrgUeuaqYQFqxLJbn-UQ3HlulFxtezn_Zq4",
+        spaceId: "398kl516wz2c",
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "123456",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
