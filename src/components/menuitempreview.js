0b413a20138cfd962af514091336a79c8421fb52
william-shelly <william-shelly@users.noreply.github.com>
2021-03-19 20:06:10 -0500

    moved menu items to menuitempreview component

--- src/components/menuitempreview.js
+++ src/components/menuitempreview.js
@@ -1,13 +1,19 @@
-export default function MenuItemPreview(props) {
+export default function MenuItemPreview(data) {
+    const menuContents = data.allContentfulMenuContents.edges;
     return (
         <>
-            {props.image ? <div className="menu-item-img col-12 col-lg-3 mb-4"><img src={props.image} alt={props.name} loading="lazy" /></div> : undefined }
-            <div className="menu-item-description col-12 col-lg-3 mb-4 text-center text-lg-left">
-                <h2>{props.name}</h2>
-                <p><span dangerouslySetInnerHTML={{ __html: props.description}}></span></p>
-                <p className="text-center text-lg-left">${props.price}</p>
-                <a class="btn btn-primarycolor d-sm-inline-block text-white" href="#0">View {props.name}</a>
-            </div>
+            {menuContents.map( ({ node, index}) => (
+                <div>
+                    {node.image.file.url ? <div className="menu-item-img col-12 col-lg-3 mb-4"><img src={node.image.file.url} alt={node.name} loading="lazy" /></div> : undefined }
+                    <div className="menu-item-description col-12 col-lg-3 mb-4 text-center text-lg-left">
+                        <h2>{node.name}</h2>
+                        <p><span dangerouslySetInnerHTML={{ __html: node.description.description}}></span></p>
+
+                        <p className="text-center text-lg-left">${node.price}</p>
+                        <a class="btn btn-primarycolor d-sm-inline-block text-white" href="#0">View {node.name}</a>
+                    </div>
+                </div>
+            ))}
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,48 @@ import '../../styles.scss'
 
 // markup
 const IndexPage = ({data}) => {
+  const businessName = 'Kilkennys';
+  const welcomePhrase = 'Welcome to ' + businessName;
   const menuContents = data.allContentfulMenuContents.edges;
   return (
     <Default>
       <main>
-
-        {menuContents.map( ({ node, index}) => (
-          <div>
-            <h2>{node.name}</h2>
-            <img src={node.image.file.url} alt={node.name} />
-            <p>{node.description.description}</p>
-              {/*<img
-              src={data.allContentfulMenuContents.nodes.image.file.url}
-              alt={data.allContentfulMenuContents.nodes.name}/> */}
+        <div className="container-fluid intro-area-container">
+          <div className="row">
+            <div className="col-sm-12 p-0 intro-area">
+              <div className="text-container p-5 d-flex flex-column justify-content-end justify-content-lg-center align-items-center">
+                <h1 className="d-none d-lg-block text-center pb-4 text-shadow">{welcomePhrase}</h1>
+                <p className="d-none d-lg-block text-center text-white text-shadow">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.</p>
+              </div>
+            </div>
           </div>
-        ))}
-
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="row">
+                <div className="col py-2 py-lg-5 d-flex justify-content-center align-items-center">
+                  <h2 className="fs-3">Menu</h2>
+                </div>
+              </div>
+              <div className="row">
+                {menuContents.map( ({ node, index}) => (
+                  <div className="col-12 col-lg-6 mb-4">
+                    <div className="row">
+                      {node.image.file.url ? <div className="menu-item-img col-12 col-lg-6"><img src={node.image.file.url} alt={node.name} loading="lazy" /></div> : undefined }
+                      <div className="menu-item-description col-12 col-lg-6 text-center text-lg-left">
+                          <h2>{node.name}</h2>
+                          <p><span dangerouslySetInnerHTML={{ __html: node.description.description}}></span></p>
+                          <p className="text-center text-lg-left">${node.price}</p>
+                          <a class="btn btn-primarycolor d-sm-inline-block text-white" href="#0">View {node.name}</a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </Default>
   )
@@ -42,11 +68,12 @@ query MyQuery {
         description {
           description
         }
+        price
+        menuContentsType
       }
     }
   }
 }
-
 `
 
 export default IndexPage
